8619f64167eb448239a4661c386814af0a4095b4
owolabi-develop <owolabidevelop84@gmail.com>
2024-03-13 17:43:58 -0700

    add cdk developement

--- real_time_sendashboad/real_time_sendashboad_stack.py
+++ real_time_sendashboad/real_time_sendashboad_stack.py
@@ -1,26 +1,40 @@
 from constructs import Construct
 from aws_cdk import (
-    Duration,
     Stack,
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw
+    
 )
+from . hitcounter import HitCounter
+from cdk_dynamo_table_view import TableViewer
 
 
 class RealTimeSendashboadStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
-        queue = sqs.Queue(
-            self, "RealTimeSendashboadQueue",
-            visibility_timeout=Duration.seconds(300),
+        
+        my_lambda = _lambda.Function(self,"helloHandler",
+                                     runtime=_lambda.Runtime.PYTHON_3_12,
+                                     code=_lambda.Code.from_asset('lambda'),
+                                     handler='hello.handler',
+                                     )
+        hello_with_count = HitCounter(
+            self,"helloCounter",
+            downstream=my_lambda,
         )
-
-        topic = sns.Topic(
-            self, "RealTimeSendashboadTopic"
+        
+        apigw.LambdaRestApi(
+            self,"Endpoint",
+            handler = hello_with_count.handler,
         )
+        
+        TableViewer(
+            self,
+            'ViewHitCounter',
+            title='Hello Hits',
+            table=hello_with_count.table
+        )
+        
 
-        topic.add_subscription(subs.SqsSubscription(queue))
+       
